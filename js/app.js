819452b15e1fdca1088f118842f0b433114a8fc0
DZ <rdavidzhong@gmail.com>
2016-01-28 21:39:13 -0800

    setup

---
+++ js/app.js
@@ -0,0 +1,10 @@
+/**
+ * Created by codename on 2016-01-28.
+ */
+
+var myApp = angular.module('myApp', []);
+
+myApp.controller('appController', ['$scope', function($scope) {
+   $scope.message = "my Registration App";
+}])
+
--- sync.sh
+++ sync.sh
@@ -1,3 +1,3 @@
 git add -A
-git commit -m init
+git commit -m setup
 git push
